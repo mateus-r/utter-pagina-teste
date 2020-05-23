53f86f439a9e2027d078bdc553028dd1503f43dd
Mateus Ramos <matteusr@live.com>
2020-05-22 21:04:25 -0300

    teste

---
+++ precache-manifest.e1211927eae29629725b90dc856657f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22d73691817991d87c7dbc2d277a49c0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c797136673f83ca5b8b3",
+    "url": "./static/css/2.e141a969.chunk.css"
+  },
+  {
+    "revision": "a51b865ab70cd7f677f0",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c797136673f83ca5b8b3",
+    "url": "./static/js/2.de9d5e67.chunk.js"
+  },
+  {
+    "revision": "4a6a2e64d793e9f3261bae43c6a4fdc4",
+    "url": "./static/js/2.de9d5e67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a51b865ab70cd7f677f0",
+    "url": "./static/js/main.60e784cb.chunk.js"
+  },
+  {
+    "revision": "fe4cbaaff829181c67d9",
+    "url": "./static/js/runtime-main.1b85c405.js"
+  }
+]);
